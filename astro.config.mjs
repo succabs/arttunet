6ce71a25eabe6c3dbb0ddd17365d5f3e4e68d7da
Arttu Nikkilä <artnikkila@gmail.com>
2025-04-21 01:12:10 +0300

    change domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://succabs.github.io',
+  site: 'https://arttu.net',
   integrations: [mdx(), sitemap()],
 });
